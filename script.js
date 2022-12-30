e926856853a4d33294ee3392cbafba8b63e4511e
davidecardelli <davide.cardelli.94@gmail.com>
2022-12-30 16:24:29 +0100

    add array

---
+++ script.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+      id: 1,
+      author: 'Phil Mangione',
+      authorPhoto: '',
+      date: '12/24/2022',
+      text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+      postPhoto: 'https://unsplash.it/600/300?image=171',
+      like: 10,
+    }, {
+        id: 2,
+        author: 'Francesco Rossi',
+        authorPhoto: '',
+        date: '12/25/2022',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.' ,
+        postPhoto: 'https://unsplash.it/600/300?image=172',
+        like: 20 ,
+    }, {
+        id: 3 ,
+        author: 'Davide Cardelli' ,
+        authorPhoto: '' ,
+        date: '12/26/2022' ,
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.' , 
+        postPhoto: 'https://unsplash.it/600/300?image=173' , 
+        like: 30 ,
+    }
+  ];
