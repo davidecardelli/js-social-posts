58c434615540d6ae0b66403ba792d84d85147bdb
davidecardelli <davide.cardelli.94@gmail.com>
2022-12-30 16:37:23 +0100

    milestone 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const data = [
+const posts = [
     {
       id: 1,
       author: 'Phil Mangione',
@@ -25,3 +25,52 @@ const data = [
         like: 30 ,
     }
   ];
+
+// Recupero container dei post dal DOM
+
+const container = document.getElementById('container');
+
+// Creo una variabile di supporto per la creazione del post
+
+let post = '';
+
+// Giro nell'array per sparare in pagina i vari post
+
+for (i = 0; i < posts.length; i ++) {
+
+  post += 
+
+  `
+  <div class="post">
+      <div class="post__header">
+        <div class="post-meta">
+          <div class="post-meta__icon">
+            <img class="profile-pic" src="${posts[i].authorPhoto}" alt="${posts[i].author}" />
+          </div>
+          <div class="post-meta__data">
+            <div class="post-meta__author">${posts[i].author}</div>
+            <div class="post-meta__time">${posts[i].date}</div>
+          </div>
+        </div>
+      </div>
+      <div class="post__text">${posts[i].text}</div>
+      <div class="post__image">
+        <img src="${posts[i].postPhoto}" alt="" />
+      </div>
+      <div class="post__footer">
+        <div class="likes js-likes">
+          <div class="likes__cta">
+            <button class="like-button js-like-button" href="#" data-postid="${posts[i].id}">
+              <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+              <span class="like-button__label">Mi Piace</span>
+            </button>
+          </div>
+          <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].like}</b> persone</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
+}
+
+container.innerHTML = post;
